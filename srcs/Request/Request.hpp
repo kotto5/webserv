88ef484553a72a791b6ecb57afd4e6627da7df04
dorakueYaritai <kotto555555@gmail.com>
2023-07-01 21:45:17 +0900

    add protocol member at Request class

--- srcs/Request/Request.hpp
+++ srcs/Request/Request.hpp
@@ -9,7 +9,7 @@ class Request
 {
 public:
 	// Constructors
-	Request(const std::string &method, const std::string &uri,
+	Request(const std::string &method, const std::string &uri, const std::string &protocol,
 			const std::map<std::string, std::string> &headers, const std::string &body);
 	Request(const std::string &row_request);
 	Request(const Request &other);
@@ -23,12 +23,14 @@ public:
 	// Getters/Setters
 	std::string getMethod() const;
 	std::string getUri() const;
+	std::string	getProtocol() const;
 	std::string getHeader(const std::string &key) const;
 	std::string getBody() const;
 
 private:
 	std::string _method;
 	std::string _uri;
+	std::string _protocol;
 	std::map<std::string, std::string> _headers;
 	std::string _body;
 
