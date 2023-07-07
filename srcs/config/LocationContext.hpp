cc0139dea5fb83023f36a892f5b6033e986786a1
sosoiwasa <souwasao084@gmail.com>
2023-07-07 12:07:08 +0900

    not important

--- srcs/config/LocationContext.hpp
+++ srcs/config/LocationContext.hpp
@@ -13,13 +13,16 @@ class LocationContext
 		void setPath(const std::string& path);
 		void setAlias(const std::string& alias);
 		void setIndex(const std::string& index);
+		//addDirective
 		void addErrorPage(const std::string& status_code, const std::string& error_page);
 		const std::string& getPath() const;	
 		const std::string& getAlias() const;
 		const std::string& getIndex() const;
 		const std::map<std::string, std::string>& getErrorPage() const;
+		//getDirective(const std::string& directive);
 		
 	private:
+		//std::map<std::string, std::string> _directives;
 		std::string _path;
 		std::string _alias;
 		std::string _index;
