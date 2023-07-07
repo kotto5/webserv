cb525f3559b86e3670bf77fd5dbe69c52112edbe
sosoiwasa <souwasao084@gmail.com>
2023-07-07 13:04:38 +0900

    directives, getDirective

--- srcs/config/LocationContext.cpp
+++ srcs/config/LocationContext.cpp
@@ -1,10 +1,8 @@
 #include "LocationContext.hpp"
 
 LocationContext::LocationContext():
-	_path(),
-	_alias(),
-	_index(),
-	_error_page()
+	_error_page(),
+	_directives()
 {
 }
 
@@ -12,42 +10,21 @@ LocationContext::~LocationContext()
 {
 }
 
-void LocationContext::setPath(const std::string& path)
-{
-	_path = path;
-}
-
-void LocationContext::setAlias(const std::string& alias)
-{
-	_alias = alias;
-}
-
-void LocationContext::setIndex(const std::string& index)
-{
-	_index = index;
-}
-
 void LocationContext::addErrorPage(const std::string& status_code, const std::string& error_page)
 {
 	_error_page.insert(std::make_pair(status_code, error_page));
 }
 
-const std::string& LocationContext::getPath() const
+void LocationContext::addDirective(const std::string& directive, const std::string& value)
 {
-	return _path;
-}
-
-const std::string& LocationContext::getAlias() const
-{
-	return _alias;
-}
-
-const std::string& LocationContext::getIndex() const
-{
-	return _index;
+	_directives.insert(std::make_pair(directive, value));
 }
 
 const std::map<std::string, std::string>& LocationContext::getErrorPage() const
 {
 	return _error_page;
+}
+const std::map<std::string, std::string>& LocationContext::getDirective(const std::string& directive)
+{
+	return _directives;
 }
--- srcs/config/LocationContext.hpp
+++ srcs/config/LocationContext.hpp
@@ -10,23 +10,14 @@ class LocationContext
 	public:
 		LocationContext();
 		~LocationContext();
-		void setPath(const std::string& path);
-		void setAlias(const std::string& alias);
-		void setIndex(const std::string& index);
-		//addDirective
 		void addErrorPage(const std::string& status_code, const std::string& error_page);
-		const std::string& getPath() const;	
-		const std::string& getAlias() const;
-		const std::string& getIndex() const;
+		void addDirective(const std::string& directive, const std::string& value);
 		const std::map<std::string, std::string>& getErrorPage() const;
-		//getDirective(const std::string& directive);
+		const std::map<std::string, std::string>& getDirective(const std::string& directive);
 		
 	private:
-		//std::map<std::string, std::string> _directives;
-		std::string _path;
-		std::string _alias;
-		std::string _index;
 		std::map<std::string, std::string> _error_page;
+		std::map<std::string, std::string> _directives;
 
 };
 
