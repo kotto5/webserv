687185667120bf24cad615e3db5f26d5ceb5410c
sosoiwasa <souwasao084@gmail.com>
2023-07-09 02:24:54 +0900

    from local susiwasa branch

--- srcs/config/LocationContext.hpp
+++ srcs/config/LocationContext.hpp
@@ -10,12 +10,17 @@ class LocationContext
 	public:
 		LocationContext();
 		~LocationContext();
+		void setPath(const std::string& path);
+		void setAlias(const std::string& alias);
+		void setIndex(const std::string& index);
 		void addErrorPage(const std::string& status_code, const std::string& error_page);
 		void addDirective(const std::string& directive, const std::string& value);
 		const std::map<std::string, std::string>& getErrorPage() const;
-		const std::map<std::string, std::string>& getDirective() const;
 		
 	private:
+		std::string _path;
+		std::string _alias;
+		std::string _index;
 		std::map<std::string, std::string> _error_page;
 		std::map<std::string, std::string> _directives;
 
