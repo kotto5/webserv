9b6bd93261413d9bc619bb553370eb515335b3d1
dorakueYaritai <kotto555555@gmail.com>
2023-08-03 18:57:37 +0900

    message to send Cgi deal as OnlyBody class which delivered Httpmessage

---
+++ srcs/HttpMassage/OnlyBody.cpp
@@ -0,0 +1,15 @@
+#include "OnlyBody.hpp"
+
+OnlyBody::OnlyBody()
+{
+    _isHeaderEnd = true;
+}
+
+void	OnlyBody::setFirstLine(const std::string &line)
+{
+    (void)line;
+}
+
+OnlyBody::~OnlyBody()
+{
+}
