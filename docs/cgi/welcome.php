7867d5085ec6c973f782473afa33bda5299d22df
dorakueYaritai <kotto555555@gmail.com>
2023-08-15 22:52:02 +0900

    welcomePhp のmodify

--- docs/cgi/welcome.php
+++ docs/cgi/welcome.php
@@ -1,4 +1,16 @@
 <?php
+echo ("HTTP/1.1 200 OK\r\n");
+echo ("Content-Type: text/html; charset=utf-8\r\n\r\n");
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>PHPテスト</title>
+</head>
+<body>
+<?php
+echo $_SERVER["REQUEST_METHOD"] , "<br>";
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
     // 標準入力からデータを読み込む
@@ -8,18 +20,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
         $input .= $line;
     }
     fclose($stdin);
-    
+
+    echo $input , "<br>";
     // データを解析し、変数に分解する
     parse_str($input, $data);
     
     // 解析した結果を出力する
     var_dump($data);
-    
+    var_dump($_GET);
+
     // 名前とEメールを取得する
     $name = urldecode($data['name']);
     $email = urldecode($data['email']);
     
-    // 出力する
+    // // 出力する
     echo "Welcome $name!<br>Your email is: $email";
 }
 ?>
+</body>
+</html>
