a86223037bc6194d1865d89932146be891acb5f5
dorakueYaritai <kotto555555@gmail.com>
2023-08-15 20:35:42 +0900

    php file で 標準入力からbody を受け取り、parse して変数dataに保存するようにした

--- docs/cgi/welcome.php
+++ docs/cgi/welcome.php
@@ -1,12 +1,25 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    if (empty($name) || empty($email)) {
-        echo "Name or email is empty";
-    } else {
-        echo "Welcome $name!<br>Your email is: $email";
+    // 標準入力からデータを読み込む
+    $stdin = fopen('php://stdin', 'r');
+    $input = '';
+    while($line = fgets($stdin)) {
+        $input .= $line;
     }
+    fclose($stdin);
+    
+    // データを解析し、変数に分解する
+    parse_str($input, $data);
+    
+    // 解析した結果を出力する
+    var_dump($data);
+    
+    // 名前とEメールを取得する
+    $name = urldecode($data['name']);
+    $email = urldecode($data['email']);
+    
+    // 出力する
+    echo "Welcome $name!<br>Your email is: $email";
 }
 ?>
