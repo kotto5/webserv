18e9ff282e19614ce018502f770feafa91ddf284
dorakueYaritai <kotto555555@gmail.com>
2023-08-25 23:01:54 +0900

    when return error response, through handleError()

--- srcs/Router/Router.cpp
+++ srcs/Router/Router.cpp
@@ -67,11 +67,11 @@ HttpMessage *Router::routeHandler(HttpMessage &message, Socket *sock)
 			.getServerContext(request->getServerPort(), request->getHeader("host"));
 
 		if (request->isBadRequest())
-			return (new Response("400"));
+			return handleError(*request, "400");
 		if (request->isTooBigError())
-			return (new Response("401"));
+			return handleError(*request, "401");
 		if (request->getUri().find("..") != std::string::npos)
-			return (new Response("403")); // ディレクトリトラバーサルの場合
+			return handleError(*request, "403"); // ディレクトリトラバーサルの場合
 		if (isRedirect(*request))
 		{
 			std::map<std::string, std::string> headers;
@@ -79,7 +79,7 @@ HttpMessage *Router::routeHandler(HttpMessage &message, Socket *sock)
 			return new Response("301", headers, "");
 		}
 		if (isAllowedMethod(*request) == false)
-			return (new Response("405"));
+			return handleError(*request, "405");
 
 		// メソッドに対応するhandlerを呼び出し
 		try
