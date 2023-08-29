cc88900efc5586b97fc1bc3ed8593f5280e2294e
dorakueYaritai <kotto555555@gmail.com>
2023-08-29 19:10:56 +0900

    play

--- play.cpp
+++ play.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
+#include <string>
 // template <typename T>
+
 // class Result
 // {
 // private:
@@ -16,3 +19,32 @@
 // Result::~Result()
 // {
 // }
+
+class Base {};
+
+class Derived : public Base {};
+
+std::string hoge(Base &b) { return ("base"); }
+std::string hoge(Derived &d) { return ("derived"); }
+
+std::string hoge(Base *b) { return ("base"); }
+std::string hoge(Derived *d) { return ("derived"); }
+
+int main()
+{
+    Base b;
+    Derived d;
+    std::cout << "b: " << hoge(b) << std::endl; // 5
+    std::cout << "d: " << hoge(d) << std::endl;  // 10
+
+    std::cout << "b: " << hoge(&b) << std::endl; // 5
+    std::cout << "d: " << hoge(&d) << std::endl; // 10
+
+	Base &base_d = d;
+	std::cout << "Base &base_d = d: " << hoge(base_d) << std::endl; // 5
+    std::cout << "((Base *)&d) : " << hoge((Base *)&d) << std::endl; // 5
+
+    std::cout << hoge(*((Base *)&d)) << std::endl; // 5
+
+    return (0);
+}
