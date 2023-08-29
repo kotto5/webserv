9dcbe04031539d30c10c206899ed734fe85c2c17
dorakueYaritai <kotto555555@gmail.com>
2023-08-29 20:43:00 +0900

    cgi response can interpret when first line is header line

---
+++ srcs/HttpMassage/CgiResponse.cpp
@@ -0,0 +1,41 @@
+#include "CgiResponse.hpp"
+#include <string>
+
+CgiResponse::CgiResponse(/* args */)
+{
+}
+
+CgiResponse::~CgiResponse()
+{
+}
+
+bool    CgiResponse::isValidLine(const std::string &line, const bool isFirstLine) const
+{
+	if (isFirstLine)
+        return (isStatusLine(line) || 
+                isHeaderLine(line));
+	else
+        return (isHeaderLine(line));
+	return (true);
+}
+
+void	CgiResponse::setStatusLine(const std::string &line)
+{
+	std::string::size_type	protocol_end = line.find(" ");
+	std::string::size_type	status_end = line.find(" ", protocol_end + 1);
+	_protocol = line.substr(0, protocol_end);
+	_status = line.substr(protocol_end + 1, status_end - protocol_end - 1);
+	_statusMessage = line.substr(status_end + 1);
+}
+
+void	CgiResponse::setFirstLine(const std::string &line)
+{
+    if (isStatusLine(line))
+    {
+        setStatusLine(line);
+    }
+    else if (isHeaderLine(line))
+    {
+        setHeaderFromLine(_headers, line, ": ");
+    }
+}
---
+++ srcs/HttpMassage/CgiResponse.hpp
@@ -0,0 +1,40 @@
+#ifndef CGIRESPONSE_HPP
+#define CGIRESPONSE_HPP
+
+#include "HttpMessage.hpp"
+#include <string>
+
+// cgi handler でいつか使うリスト　とっておいて
+
+// document-response = Content-Type [ Status ] *other-field NL response-body
+// local-redir-response = local-Location NL
+// client-redir-response = client-Location *extension-field NL
+//  client-redirdoc-response = client-Location Status Content-Type *other-field NL response-body
+
+// local-Location  = "Location:" local-pathquery NL
+// client-Location = "Location:" fragment-URI NL
+    // enum Type
+    // {
+    //     DocumentResponse,
+    //     LocalRedirectResponse,
+    //     ClientRedirectResponse,
+    //     ClientRedirectResponseWithDocument
+    // };
+    // Type _type;
+
+
+class CgiResponse: public HttpMessage
+{
+private:
+	void	setFirstLine(const std::string &line);
+	bool	isValidLine(const std::string &line, const bool isFirstLine) const;
+    void	setStatusLine(const std::string &line);
+	std::string _protocol;
+	std::string _status;
+	std::string	_statusMessage;
+public:
+    CgiResponse(/* args */);
+    ~CgiResponse();
+};
+
+#endif
