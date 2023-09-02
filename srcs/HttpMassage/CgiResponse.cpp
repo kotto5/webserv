6e7e464261841f9b068fdbe3d962f8502b329be3
dorakueYaritai <kotto555555@gmail.com>
2023-09-02 16:24:08 +0900

    remove first line function

--- srcs/HttpMassage/CgiResponse.cpp
+++ srcs/HttpMassage/CgiResponse.cpp
@@ -14,14 +14,6 @@ bool    CgiResponse::isValidLine(const std::string &line, const bool isFirstLine
     return (isHeaderField(line));
 }
 
-void	CgiResponse::setStatusLine(const std::string &line)
-{
-	std::string::size_type	protocol_end = line.find(" ");
-	std::string::size_type	status_end = line.find(" ", protocol_end + 1);
-	_protocol = line.substr(0, protocol_end);
-	_status = line.substr(protocol_end + 1, status_end - protocol_end - 1);
-	_statusMessage = line.substr(status_end + 1);
-}
 
 void	CgiResponse::setFirstLine(const std::string &line)
 {
