7446a3d2f026dd966a612b4a18c904b0682bd43b
dorakueYaritai <kotto555555@gmail.com>
2023-09-07 21:39:45 +0900

    play

--- play.cpp
+++ play.cpp
@@ -95,27 +95,101 @@
 // }
 
 
-#include <sstream>
-#include <iostream>
+// #include <sstream>
+// #include <iostream>
+// #include <string>
+
+// int main()
+// {
+//     {
+//         std::stringstream ss("3a4ga");
+//         unsigned int a;
+//         ss >> std::hex >> a;
+//         std::cout << a << std::endl;
+
+//         std::string b;
+//         ss >> b;
+//         std::cout << b << std::endl;
+//     }
+//     std::cout << "---------" << std::endl;
+//     {
+//         std::stringstream ss("3a4ga");
+//         std::string a;
+//         ss >> std::hex >> a;
+//         std::cout << a << std::endl;
+//     }
+// }
+
+// /cgi/echo.php/aaa/bbb
+// pathInfo: /aaa/bbb
+// pathTranslated: /Users/ryoji/Desktop/42cursus/webserv/webserv/htdocs/cgi/echo.php/aaa/bbb
+// scriptName: /echo.php
+// directory /cgi
+
 #include <string>
+#include <iostream>
+
+#include <string>
+#include <iostream>
+
+std::string makePathInfo(const std::string& str) {
+    std::string::size_type pos = str.find(".php");
+    if (pos == std::string::npos) {
+        return ""; // ".php" が見つからない場合、空の文字列を返す
+    }
+    pos += 4; // ".php" の長さ分だけ位置を移動
+
+    // pos 以降の文字列を返す
+    if (pos < str.size()) {
+        return str.substr(pos);
+    } else {
+        return "";
+    }
+}
+
+
+std::string makeScriptName(const std::string& str) {
+    std::string::size_type pos = str.rfind(".php");
+    if (pos == std::string::npos) {
+        return "";
+    }
+    std::string::size_type startPos = str.rfind('/', pos);
+    if (startPos == std::string::npos) {
+        return "";
+    }
+    return str.substr(startPos, pos + 4 - startPos);
+}
+
+std::string makeDirectory(const std::string& str) {
+    std::string::size_type pos = str.rfind(".php");
+    if (pos == std::string::npos) {
+        return "";
+    }
+    std::string::size_type endPos = str.rfind('/', pos);
+    if (endPos == std::string::npos) {
+        return "";
+    }
+    std::string::size_type startPos = str.rfind('/', endPos - 1);
+    if (startPos == std::string::npos) {
+        return "";
+    }
+    return str.substr(startPos, endPos - startPos);
+}
 
 int main()
 {
-    {
-        std::stringstream ss("3a4ga");
-        unsigned int a;
-        ss >> std::hex >> a;
-        std::cout << a << std::endl;
+    std::string str = "/cgi/echo.php/aaa/bbb";
+    // .php で終わる部分以降の部分を切り取る
+    std::string pathInfo = makePathInfo(str);
+    std::cout << "pathInfo: " << pathInfo << std::endl; // /aaa/bbb
+    // .php のscriptName を取得する
+    std::string scriptName = makeScriptName(str); // /echo.php
+    std::cout << "scriptName: " << scriptName << std::endl;
 
-        std::string b;
-        ss >> b;
-        std::cout << b << std::endl;
-    }
-    std::cout << "---------" << std::endl;
-    {
-        std::stringstream ss("3a4ga");
-        std::string a;
-        ss >> std::hex >> a;
-        std::cout << a << std::endl;
-    }
+    // 移動するべきディレクトリを取得する
+    std::string directory = makeDirectory(str); // /cgi
+    std::cout << "directory " << directory << std::endl;
+
+    return 0;
 }
+
