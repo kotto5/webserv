868fb7001b6bef9b76d9c41bf52c7e874077dfd1
tmuramat <mt15hydrangea@gmail.com>
2023-09-12 00:03:03 +0900

    FIX: スクリプト微修正

--- docs/cgi-py/index.py
+++ docs/cgi-py/index.py
@@ -1,7 +1,10 @@
-print("Content-type: text/html")
-print("Status: 200 OK")
+import sys
 
-print("<html><head><title>Test CGI Script</title></head>")
-print("<body>")
-print("<h1>Hello, World!</h1>")
-print("</body></html>")
+sys.stdout.write("Content-type: text/html\r\n")
+sys.stdout.write("Status: 200 OK\r\n")
+sys.stdout.write("\r\n")
+
+sys.stdout.write("<html><head><title>Test CGI Script</title></head>")
+sys.stdout.write("<body>")
+sys.stdout.write("<h1>Hello, World!</h1>")
+sys.stdout.write("</body></html>")
